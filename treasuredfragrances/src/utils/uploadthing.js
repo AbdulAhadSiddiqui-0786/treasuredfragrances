2af4beca4d84b60e1f39a75e92a962e6c34b9552
AbdulAhadSiddiqui-0786 <abdulahadsiddiqui23@gmail.com>
2025-10-31 00:44:08 +0530

    admin

--- treasuredfragrances/src/utils/uploadthing.js
+++ treasuredfragrances/src/utils/uploadthing.js
@@ -3,5 +3,5 @@ import { generateReactHelpers } from "@uploadthing/react";
 const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 export const { useUploadThing, uploadFiles } = generateReactHelpers({
-  url: `${URL}/api/uploadthing`,
+  url: `${URL}/uploadthing`,
 });
